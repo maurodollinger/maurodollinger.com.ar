c611d25adcb45a941a440c09f574ed6275c7b5ab
Mauro <dollingermauro@gmail.com>
2026-01-06 15:13:57 -0300

    update fixing conflicts

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,9 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = 'Espacio Estelar - Mauro Dollinger'
+                args[0].title = 'Mauro Dollinger - Web Portfolio'
                 return args
             })
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 }
